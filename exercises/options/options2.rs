d645dfe5d18ddaeba5f6a2c9f051602a36ea4172
quanguanlangze <sandy_chat_yvvone@foxmail.com>
2024-10-20 22:42:12 +0800

    50 slove

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -1,10 +1,3 @@
-// options2.rs
-//
-// Execute `rustlings hint options2` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
@@ -12,8 +5,8 @@ mod tests {
         let target = "rustlings";
         let optional_target = Some(target);
 
-        // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
+        // Use if let to check if optional_target is Some and extract the value
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
         }
     }
@@ -29,10 +22,8 @@ mod tests {
 
         let mut cursor = range;
 
-        // TODO: make this a while let statement - remember that vector.pop also
-        // adds another layer of Option<T>. You can stack `Option<T>`s into
-        // while let and if let.
-        integer = optional_integers.pop() {
+        // Use while let to keep popping elements until the vector is empty
+        while let Some(Some(integer)) = optional_integers.pop() {
             assert_eq!(integer, cursor);
             cursor -= 1;
         }
